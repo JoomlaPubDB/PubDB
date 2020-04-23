508d797cae5bc2e04d6d3fa0f3142c66c2e3707b
JulianPfau <pfau.julian-tim17@it.dhbw-ravensburg.de>
2020-04-23 10:47:10 +0200

    added new functionality to use custom format functions

--- administrator/helpers/citations.php
+++ administrator/helpers/citations.php
@@ -24,85 +24,98 @@ $blocks = null;
 class PubdbLiteraturesCitation
 {
 
-    /**
-     * Was passiert hier
-     *
-     * @param $patternId
-     * @param $literatureList
-     * @since  0.0.1
-     */
-    public function mapList($patternId, $literatureList)
-    {
-        global $blocks;
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
+  /**
+   * Was passiert hier
+   *
+   * @param $patternId
+   * @param $literatureList
+   * @since  0.0.1
+   */
+  public function mapList($patternId, $literatureList)
+  {
+    global $blocks;
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
 
-        $query
-            ->select('*')
-            ->from($db->quoteName('#__pubdb_blocks'));
-        $db->setQuery($query);
-        $blocks = $db->loadAssocList('id');
+    $query
+      ->select('*')
+      ->from($db->quoteName('#__pubdb_blocks'));
+    $db->setQuery($query);
+    $blocks = $db->loadAssocList('id');
 
-        $pattern = self::getPatternFromDb($patternId);
-        $res = array();
+    $pattern = self::getPatternFromDb($patternId);
+    $res = array();
 
-        foreach ($literatureList as $entry) {
-            $res[] = self::mapEntry($pattern, $entry);
-        }
-
-        return $res;
+    foreach ($literatureList as $entry) {
+      $res[] = self::mapEntry($pattern, $entry);
     }
 
-    /**
-     * Was passiert hier
-     *
-     * @param $pattern
-     * @param $entry
-     *
-     * @return String citation
-     *
-     * @since  0.0.1
-     */
-    private function mapEntry($pattern, $entry)
-    {
-        global $blocks;
-        $result = "";
-        $ref_type = $entry->reference_type;
-        // Block is int id from block_db
-        foreach ((array) $pattern[$ref_type] as $k => $block) {
-            $content = strtolower($blocks[$block]['name']);
-            $prefix = $blocks[$block]['prefix'];
-            $suffix = $blocks[$block]['suffix'];
-            if ($content != null) {
-                if ($prefix != null) $result .= $prefix;
-                $result .= $entry->$content;
-                if ($suffix != null) $result .= $suffix;
-                $result .= " ";
-            }
-        }
-        return $result;
-    }
+    return $res;
+  }
 
-    /**
-     * Was passiert hier
-     *
-     * @param $citation_style_id
-     *
-     * @return String pattern
-     *
-     * @since  0.0.1
-     */
-
-    private function getPatternFromDb($citation_style_id) {
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query
-            ->select($db->qn('string'))
-            ->from($db->quoteName('#__pubdb_citation_style'))
-            ->where($db->quoteName('id') . ' = ' . (int) $citation_style_id);
-        $db->setQuery($query);
-        $pattern = $db->loadResult();
-        $pattern = json_decode($pattern,1);
-        return $pattern;
+  /**
+   * Was passiert hier
+   *
+   * @param $pattern
+   * @param $entry
+   *
+   * @return String citation
+   *
+   * @since  0.0.1
+   */
+  private function mapEntry($pattern, $entry)
+  {
+    global $blocks;
+    $result = "";
+    $ref_type = $entry->reference_type;
+    // Block is int id from block_db
+    foreach ((array)$pattern[$ref_type] as $k => $block) {
+      $blockName = ucfirst(strtolower($blocks[$block]['name']));
+      print($blockName);
+      var_dump(method_exists(self::class, 'format' . $blockName . 'Field'));
+      if (method_exists(self::class, 'format' . $blockName . 'Field')) {
+        $content = call_user_func(array(__CLASS__, 'format' . $blockName . 'Field'), $entry);
+      } else {
+        $content = strtolower($blocks[$block]['name']);
+      }
+      $prefix = $blocks[$block]['prefix'];
+      $suffix = $blocks[$block]['suffix'];
+      if ($content != null) {
+        if ($prefix != null) $result .= $prefix;
+        $result .= $entry->$content;
+        if ($suffix != null) $result .= $suffix;
+        $result .= " ";
+      }
     }
+    return $result;
+  }
+
+  /**
+   * Was passiert hier
+   *
+   * @param $citation_style_id
+   *
+   * @return String pattern
+   *
+   * @since  0.0.1
+   */
+
+  private function getPatternFromDb($citation_style_id)
+  {
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query
+      ->select($db->qn('string'))
+      ->from($db->quoteName('#__pubdb_citation_style'))
+      ->where($db->quoteName('id') . ' = ' . (int)$citation_style_id);
+    $db->setQuery($query);
+    $pattern = $db->loadResult();
+    $pattern = json_decode($pattern, 1);
+    return $pattern;
+  }
+
+  private function formatAuthorsField($item)
+  {
+    $arrAuthors = explode(",", $item->author_last_name);
+  }
 }

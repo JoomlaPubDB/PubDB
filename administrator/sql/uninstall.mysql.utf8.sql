c4bb236bbde8f1e48fb56cde29705991e1285c43
rost <rost@infolox.de>
2020-04-28 16:58:14 +0200

    enable installation of view, after complete reinstall com_pubDB view is created

--- administrator/sql/uninstall.mysql.utf8.sql
+++ administrator/sql/uninstall.mysql.utf8.sql
@@ -7,5 +7,6 @@ DROP TABLE IF EXISTS `#__pubdb_keywords`;
 DROP TABLE IF EXISTS `#__pubdb_citation_style`;
 DROP TABLE IF EXISTS `#__pubdb_reference_types`;
 DROP TABLE IF EXISTS `#__pubdb_blocks`;
+DROP VIEW IF EXISTS `#__pubdb_publication_list`;
 
 DELETE FROM `#__content_types` WHERE (type_alias LIKE 'com_pubdb.%');

dfe2a41c9a2f11956ba49e12d150d435506b0510
JulianPfau <pfau.julian-tim17@it.dhbw-ravensburg.de>
2020-05-01 11:24:09 +0200

    set up database js with real data and changed publication list view added bibtex example file see clickdummi/plublication_exmaple.bib

--- administrator/helpers/citations.php
+++ administrator/helpers/citations.php
@@ -24,207 +24,207 @@ $blocks = null;
 class PubdbLiteraturesCitation
 {
 
-    /**
-     * Was passiert hier
-     *
-     * @param $patternId
-     * @param $literatureList
-     * @since  0.0.1
-     */
-    public function mapList($patternId, $literatureList)
-    {
-        global $blocks;
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
+  /**
+   * Was passiert hier
+   *
+   * @param $patternId
+   * @param $literatureList
+   * @since  0.0.1
+   */
+  public function mapList($patternId, $literatureList)
+  {
+    global $blocks;
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
 
-        $query
-            ->select('*')
-            ->from($db->quoteName('#__pubdb_blocks'));
-        $db->setQuery($query);
-        $blocks = $db->loadAssocList('id');
+    $query
+      ->select('*')
+      ->from($db->quoteName('#__pubdb_blocks'));
+    $db->setQuery($query);
+    $blocks = $db->loadAssocList('id');
 
-        $pattern = self::getPatternFromDb($patternId);
-        $res = array();
+    $pattern = self::getPatternFromDb($patternId);
+    $res = array();
 
-        foreach ($literatureList as $entry) {
-            $res[] = self::mapEntry($pattern, $entry);
-        }
-
-        return $res;
+    foreach ($literatureList as $entry) {
+      $res[] = self::mapEntry($pattern, $entry);
     }
 
-    /**
-     * Was passiert hier
-     *
-     * @param $pattern
-     * @param $entry
-     *
-     * @return String citation
-     *
-     * @since  0.0.1
-     */
-    private function mapEntry($pattern, $entry)
-    {
-        $result = "";
-        $ref_type_id = self::getRefIdFromDb($entry->ref_type);
+    return $res;
+  }
 
-        $author_state = 0;
+  /**
+   * Was passiert hier
+   *
+   * @param $pattern
+   * @param $entry
+   *
+   * @return String citation
+   *
+   * @since  0.0.1
+   */
+  private function mapEntry($pattern, $entry)
+  {
+    $result = "";
+    $ref_type_id = self::getRefIdFromDb($entry->ref_type);
+
+    $author_state = 0;
+    $next_is_delimiter = false;
+    $first_is_empty = false;
+    $delimiter = null;
+    $repetition_amount = -1;
+
+    $is_in_rep = false;
+    $person_blocks = [];
+    // Block is int id from block_  db
+    for ($i = 0; $i < count((array)$pattern[$ref_type_id]); $i++) {
+      $block = $pattern[$ref_type_id][$i];
+
+      //Repetition logic
+      if ($block == 1) {
+        $is_in_rep = true;
+        $repetition_amount = count(explode(',', $entry->authors_first_name));
+      }
+
+      if ($block == 2) {
+        $next_is_delimiter = true;
+        $first_is_empty = ($pattern[$ref_type_id][$i - 1] == 1);
+      } elseif ($next_is_delimiter) {
+        $delimiter = self::getFormattedBlock($block, $entry);
         $next_is_delimiter = false;
-        $first_is_empty = false;
-        $delimiter = null;
-        $repetition_amount = -1;
+        continue;
+      }
 
-        $is_in_rep = false;
-        $person_blocks = [];
-        // Block is int id from block_  db
-        for ($i = 0; $i < count((array)$pattern[$ref_type_id]); $i++) {
-            $block = $pattern[$ref_type_id][$i];
+      if ($is_in_rep) {
+        //Cash data for repetition
+        array_push($person_blocks, $block);
 
-            //Repetition logic
-            if ($block == 1) {
-                $is_in_rep = true;
-                $repetition_amount = count(explode(',', $entry->authors_first_name));
-            }
+        //Fill on end with repetition
+        if ($block == 4) {
+          $is_in_rep = false;
 
-            if ($block == 2) {
-                $next_is_delimiter = true;
-                $first_is_empty = ($pattern[$ref_type_id][$i - 1] == 1);
-            } elseif ($next_is_delimiter) {
-                $delimiter = self::getFormattedBlock($block, $entry);
-                $next_is_delimiter = false;
+          //For each person
+          for ($j = 0; $j < $repetition_amount; $j++) {
+            //For each block
+            foreach ($person_blocks as $person_block) {
+              //Is repetition block
+              if ($person_block <= 4) {
+                $author_state = $person_block;
                 continue;
+              }
+              //Is a content block
+              if (27 <= $person_block && $person_block <= 38)
+                $person_part = explode(',', self::getFormattedBlock($person_block, $entry))[$j];
+              //Is a format block
+              else
+                $person_part = self::getFormattedBlock($person_block, $entry);
+
+              switch ($author_state) {
+                case 1:
+                  if ($j == 0 && !$first_is_empty) $result .= $person_part;
+                  break;
+                case 2:
+                  if ($j == 0 && $first_is_empty) $result .= $person_part;
+                  if ($j != 0 && $j != $repetition_amount - 1) $result .= $person_part;
+                  break;
+                case 3:
+                  //Last
+                  if ($j != 0 && $j == $repetition_amount - 1) $result .= $person_part;
+                  break;
+              }
             }
+            if ($j != $repetition_amount - 1) $result .= $delimiter;
+          }
 
-            if ($is_in_rep) {
-                //Cash data for repetition
-                array_push($person_blocks, $block);
-
-                //Fill on end with repetition
-                if ($block == 4) {
-                    $is_in_rep = false;
-
-                    //For each person
-                    for ($j = 0; $j < $repetition_amount; $j++) {
-                        //For each block
-                        foreach ($person_blocks as $person_block) {
-                            //Is repetition block
-                            if ($person_block <= 4) {
-                                $author_state = $person_block;
-                                continue;
-                            }
-                            //Is a content block
-                            if (27 <= $person_block && $person_block <= 38)
-                                $person_part = explode(',', self::getFormattedBlock($person_block, $entry))[$j];
-                            //Is a format block
-                            else
-                                $person_part = self::getFormattedBlock($person_block, $entry);
-
-                            switch ($author_state) {
-                                case 1:
-                                    if ($j == 0 && !$first_is_empty) $result .= $person_part;
-                                    break;
-                                case 2:
-                                    if ($j == 0 && $first_is_empty) $result .= $person_part;
-                                    if ($j != 0 && $j != $repetition_amount - 1) $result .= $person_part;
-                                    break;
-                                case 3:
-                                    //Last
-                                    if ($j != 0 && $j == $repetition_amount - 1) $result .= $person_part;
-                                    break;
-                            }
-                        }
-                        if ($j != $repetition_amount - 1) $result .= $delimiter;
-                    }
-
-                    //Reset
-                    $author_state = 0;
-                    $next_is_delimiter = false;
-                    $first_is_empty = false;
-                    $delimiter = null;
-                    $repetition_amount = -1;
-                }
-                continue;
-            }
-
-            $result .= self::getFormattedBlock($block, $entry);
+          //Reset
+          $author_state = 0;
+          $next_is_delimiter = false;
+          $first_is_empty = false;
+          $delimiter = null;
+          $repetition_amount = -1;
         }
-        return $result;
+        continue;
+      }
+
+      $result .= self::getFormattedBlock($block, $entry);
     }
+    return $result;
+  }
 
-    /**
-     * Was passiert hier
-     *
-     * @param $block
-     * @param $entry
-     * @return String
-     *
-     * @since  0.0.1
-     */
+  /**
+   * Was passiert hier
+   *
+   * @param $block
+   * @param $entry
+   * @return String
+   *
+   * @since  0.0.1
+   */
 
-    private function getFormattedBlock($block, $entry)
-    {
-        global $blocks;
-        $result = "";
+  private function getFormattedBlock($block, $entry)
+  {
+    global $blocks;
+    $result = "";
 
-        //Has format function
-        $blockName = ucfirst(strtolower($blocks[$block]['name']));
-        if (method_exists(self::class, 'format' . $blockName . 'Field'))
-            $content = call_user_func(array(__CLASS__, 'format' . $blockName . 'Field'), $entry);
-        //get from DB
-        else
-            $content = strtolower($blocks[$block]['name']);
+    //Has format function
+    $blockName = ucfirst(strtolower($blocks[$block]['name']));
+    if (method_exists(self::class, 'format' . $blockName . 'Field'))
+      $content = call_user_func(array(__CLASS__, 'format' . $blockName . 'Field'), $entry);
+    //get from DB
+    else
+      $content = strtolower($blocks[$block]['name']);
 
-        //Content is from DB or just format (e.g. "," or ":")
-        if ($content != null && array_key_exists($content, (array)$entry))
-            $result .= $entry->$content;
-        else
-            $result .= $content . " ";
+    //Content is from DB or just format (e.g. "," or ":")
+    if ($content != null && array_key_exists($content, (array)$entry))
+      $result .= $entry->$content;
+    else
+      $result .= $content . " ";
 
-        return $result;
-    }
+    return $result;
+  }
 
-    /**
-     * Was passiert hier
-     *
-     * @param $citation_style_id
-     *
-     * @return String pattern
-     *
-     * @since  0.0.1
-     */
+  /**
+   * Was passiert hier
+   *
+   * @param $citation_style_id
+   *
+   * @return String pattern
+   *
+   * @since  0.0.1
+   */
 
-    private function getPatternFromDb($citation_style_id)
-    {
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query
-            ->select($db->qn('string'))
-            ->from($db->quoteName('#__pubdb_citation_style'))
-            ->where($db->quoteName('id') . ' = ' . (int)$citation_style_id);
-        $db->setQuery($query);
-        $pattern = $db->loadResult();
-        $pattern = json_decode($pattern, 1);
-        return $pattern;
-    }
+  private function getPatternFromDb($citation_style_id)
+  {
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query
+      ->select($db->qn('string'))
+      ->from($db->quoteName('#__pubdb_citation_style'))
+      ->where($db->quoteName('id') . ' = ' . (int)$citation_style_id);
+    $db->setQuery($query);
+    $pattern = $db->loadResult();
+    $pattern = json_decode($pattern, 1);
+    return $pattern;
+  }
 
-    /**
-     * Was passiert hier
-     *
-     * @param $ref_type
-     * @return String pattern
-     *
-     * @since  0.0.1
-     */
+  /**
+   * Was passiert hier
+   *
+   * @param $ref_type
+   * @return String pattern
+   *
+   * @since  0.0.1
+   */
 
-    private function getRefIdFromDb($ref_type)
-    {
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query
-            ->select($db->qn('id'))
-            ->from($db->quoteName('#__pubdb_reference_types'))
-            ->where($db->quoteName('name') . ' LIKE ' . $db->quote($ref_type));
-        $db->setQuery($query);
-        return $db->loadResult();
-    }
+  private function getRefIdFromDb($ref_type)
+  {
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query
+      ->select($db->qn('id'))
+      ->from($db->quoteName('#__pubdb_reference_types'))
+      ->where($db->quoteName('name') . ' LIKE ' . $db->quote($ref_type));
+    $db->setQuery($query);
+    return $db->loadResult();
+  }
 }
--- site/models/publicationlists.php
+++ site/models/publicationlists.php
@@ -24,221 +24,239 @@ jimport('joomla.application.component.modellist');
  */
 class PubdbModelPublicationlists extends \Joomla\CMS\MVC\Model\ListModel
 {
-    /**
-     * Constructor.
-     *
-     * @param   array  $config  An optional associative array of configuration settings.
-     *
-     * @see        JController
-     * @since      1.6
-     */
-    public function __construct($config = array())
+  /**
+   * Constructor.
+   *
+   * @param   array  $config  An optional associative array of configuration settings.
+   *
+   * @see        JController
+   * @since      1.6
+   */
+  public function __construct($config = array())
+  {
+    if (empty($config['filter_fields']))
     {
-        if (empty($config['filter_fields']))
-        {
-            $config['filter_fields'] = array(
+      $config['filter_fields'] = array(
 
-            );
-        }
-
-        parent::__construct($config);
+      );
     }
 
+    parent::__construct($config);
+  }
 
 
-    /**
-     * Method to auto-populate the model state.
-     *
-     * Note. Calling getState in this method will result in recursion.
-     *
-     * @param   string  $ordering   Elements order
-     * @param   string  $direction  Order direction
-     *
-     * @return void
-     *
-     * @throws Exception
-     *
-     * @since    1.6
-     */
-    protected function populateState($ordering = null, $direction = null)
+
+  /**
+   * Method to auto-populate the model state.
+   *
+   * Note. Calling getState in this method will result in recursion.
+   *
+   * @param   string  $ordering   Elements order
+   * @param   string  $direction  Order direction
+   *
+   * @return void
+   *
+   * @throws Exception
+   *
+   * @since    1.6
+   */
+  protected function populateState($ordering = null, $direction = null)
+  {
+    $app  = JFactory::getApplication();
+    $list = $app->getUserState($this->context . '.list');
+
+    $ordering  = isset($list['filter_order'])     ? $list['filter_order']     : null;
+    $direction = isset($list['filter_order_Dir']) ? $list['filter_order_Dir'] : null;
+    if(empty($ordering)){
+      $ordering = $app->getUserStateFromRequest($this->context . '.filter_order', 'filter_order', $app->get('filter_order'));
+      if (!in_array($ordering, $this->filter_fields))
+      {
+        $ordering = "a.id";
+      }
+      $this->setState('list.ordering', $ordering);
+    }
+    if(empty($direction))
     {
-        $app  = JFactory::getApplication();
-        $list = $app->getUserState($this->context . '.list');
-
-        $ordering  = isset($list['filter_order'])     ? $list['filter_order']     : null;
-        $direction = isset($list['filter_order_Dir']) ? $list['filter_order_Dir'] : null;
-        if(empty($ordering)){
-            $ordering = $app->getUserStateFromRequest($this->context . '.filter_order', 'filter_order', $app->get('filter_order'));
-            if (!in_array($ordering, $this->filter_fields))
-            {
-                $ordering = "a.id";
-            }
-            $this->setState('list.ordering', $ordering);
-        }
-        if(empty($direction))
-        {
-            $direction = $app->getUserStateFromRequest($this->context . '.filter_order_Dir', 'filter_order_Dir', $app->get('filter_order_Dir'));
-            if (!in_array(strtoupper($direction), array('ASC', 'DESC', '')))
-            {
-                $direction = "ASC";
-            }
-            $this->setState('list.direction', $direction);
-        }
-
-        $list['limit']     = $app->getUserStateFromRequest($this->context . '.list.limit', 'limit', $app->get('list_limit'), 'uint');
-        $list['start']     = $app->input->getInt('start', 0);
-        $list['ordering']  = $ordering;
-        $list['direction'] = $direction;
-
-        $app->setUserState($this->context . '.list', $list);
-        $app->input->set('list', null);
-
-
-        // List state information.
-
-        parent::populateState($ordering, $direction);
-
-        $context = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
-        $this->setState('filter.search', $context);
-
-        // Split context into component and optional section
-        $parts = FieldsHelper::extract($context);
-
-        if ($parts)
-        {
-            $this->setState('filter.component', $parts[0]);
-            $this->setState('filter.section', $parts[1]);
-        }
+      $direction = $app->getUserStateFromRequest($this->context . '.filter_order_Dir', 'filter_order_Dir', $app->get('filter_order_Dir'));
+      if (!in_array(strtoupper($direction), array('ASC', 'DESC', '')))
+      {
+        $direction = "ASC";
+      }
+      $this->setState('list.direction', $direction);
     }
 
-    /**
-     * Build an SQL query to load the list data.
-     *
-     * @return   JDatabaseQuery
-     *
-     * @since    1.6
-     */
-    protected function getListQuery()
+    $list['limit']     = $app->getUserStateFromRequest($this->context . '.list.limit', 'limit', $app->get('list_limit'), 'uint');
+    $list['start']     = $app->input->getInt('start', 0);
+    $list['ordering']  = $ordering;
+    $list['direction'] = $direction;
+
+    $app->setUserState($this->context . '.list', $list);
+    $app->input->set('list', null);
+
+
+    // List state information.
+
+    parent::populateState($ordering, $direction);
+
+    $context = $this->getUserStateFromRequest($this->context.'.filter.search', 'filter_search');
+    $this->setState('filter.search', $context);
+
+    // Split context into component and optional section
+    $parts = FieldsHelper::extract($context);
+
+    if ($parts)
     {
-        // Create a new query object.
-
-        $db    = $this->getDbo();
-        $query = $db->getQuery(true);
-        // Create select statement
-        $query->select('*');
-        $query->from($db->quoteName('#__pubdb_publication_list'));
-        $db->setQuery($query);
-        $params = $this->state->get('parameters.menu');
-        $preFilter = $this->getResultFromParams($params);
-        //get id's as comma separated list
-        $filteredIds = implode(',', $preFilter);
-        $filterQuery = $this->getResultsFromFilters($this->state);
-        return $query;
+      $this->setState('filter.component', $parts[0]);
+      $this->setState('filter.section', $parts[1]);
     }
+  }
 
-    /**
-     * Query database with Back End menu settings to get a pre filtering
-     * @param $menu_item
-     * @return mixed
-     */
+  /**
+   * Build an SQL query to load the list data.
+   *
+   * @return   JDatabaseQuery
+   *
+   * @since    1.6
+   */
+  protected function getListQuery()
+  {
+    // Create a new query object.
 
-    private function getResultFromParams($menu_item){
-        $db = $this->getDbo();
-        $query = $db->getQuery(true);
-        $query->select($db->quoteName('id'));
-        $query->from($db->quoteName('#__pubdb_publication_list'));
+    $db    = $this->getDbo();
+    $query = $db->getQuery(true);
+    // Create select statement
+    $query->select('*');
+    $query->from($db->quoteName('#__pubdb_publication_list'));
+    $db->setQuery($query);
+    $params = $this->state->get('parameters.menu');
+    $preFilter = $this->getResultFromParams($params);
+    //get id's as comma separated list
+    $filteredIds = implode(',', $preFilter);
+    $filterQuery = $this->getResultsFromFilters($this->state);
+    return $query;
+  }
 
-        //extract menu params
-        foreach( (array) $menu_item as $param_list) {
-            //loop to get params from menu item
-            foreach ((array)$param_list as $key => $filter) {
-                //filter custom settings to not use joomla items
-                if (strpos($key, 'pubdb-') !== 0) continue;
-                // arrays need different function for sql statements
-                $key_field = explode('-', $key);
-                if (is_array($filter)) {
-                    $query->where($db->quoteName($key_field[1]) . ' IN (' . implode(',', $filter) . ')', 'AND');
-                } else {
-                    $query->where($db->quoteName($key_field[1]) . ' = ' . $filter, 'AND');
-                }
+  /**
+   * Query database with Back End menu settings to get a pre filtering
+   * @param $menu_item
+   * @return mixed
+   */
 
-            }
+  private function getResultFromParams($menu_item){
+    $db = $this->getDbo();
+    $query = $db->getQuery(true);
+    $query->select($db->quoteName('id'));
+    $query->from($db->quoteName('#__pubdb_publication_list'));
+
+    //extract menu params
+    foreach( (array) $menu_item as $param_list) {
+      //loop to get params from menu item
+      foreach ((array)$param_list as $key => $filter) {
+        //filter custom settings to not use joomla items
+        if (strpos($key, 'pubdb-') !== 0) continue;
+        // arrays need different function for sql statements
+        $key_field = explode('-', $key);
+        if (is_array($filter)) {
+          $query->where($db->quoteName($key_field[1]) . ' IN (' . implode(',', $filter) . ')', 'AND');
+        } else {
+          $query->where($db->quoteName($key_field[1]) . ' = ' . $filter, 'AND');
         }
 
-        $db->setQuery($query);
-
-        return $db->loadColumn();
+      }
     }
 
-    private function getResultsFromFilters($state){
-        $arrFilter = array();
+    $db->setQuery($query);
 
-        // go through all fields and keep only relevant ones
-        foreach ( (array) $state as $filter_field => $value){
-            //ignore non filter fields
-            if (strpos($filter_field, 'filter') !== 0) continue;
-            //ignore empty values
-            //if (trim($value) == "") continue;
-            $arrFilter[$filter_field] = $value;
-        }
+    return $db->loadColumn();
+  }
 
-        print_r($arrFilter);
+  private function getResultsFromFilters($state){
+    $arrFilter = array();
 
+    // go through all fields and keep only relevant ones
+    foreach ( (array) $state as $filter_field => $value){
+      //ignore non filter fields
+      if (strpos($filter_field, 'filter') !== 0) continue;
+      //ignore empty values
+      //if (trim($value) == "") continue;
+      $arrFilter[$filter_field] = $value;
     }
 
-    /**
-     * Method to get an array of data items
-     *
-     * @return  mixed An array of data on success, false on failure.
-     */
-    public function getItems()
+  }
+
+  /**
+   * Method to get an array of data items
+   *
+   * @return  mixed An array of data on success, false on failure.
+   */
+  public function getItems()
+  {
+    $items = parent::getItems();
+    $params = $this->state->get('parameters.menu');
+    $pattern = $params['citation_style_id'][0];
+    $formattedStrings = PubdbLiteraturesCitation::mapList($pattern, $items);
+    $arrReturn = array();
+    // build json object
+    for ($i = 0 ; $i < count($items); $i ++){
+      $tmpItem = (array) $items[$i];
+      $tmpItem['formatted_string'] = $formattedStrings[$i];
+      $tmpItem['authors'] = $this->getAuthorsFromItem($tmpItem);
+      $arrReturn[] = $tmpItem;
+    }
+    return $arrReturn;
+  }
+
+
+  private function getAuthorsFromItem($item){
+    $last_names = explode(',', $item['authors_last_name']);
+    $first_names = explode(',' , $item['authors_first_name']);
+    $arrAuthors = array();
+    for($i = 0; $i  < count($last_names); $i ++){
+      $arrAuthors[] = $last_names[$i]. " " . $first_names[$i];
+    }
+    return $arrAuthors;
+  }
+
+  /**
+   * Overrides the default function to check Date fields format, identified by
+   * "_dateformat" suffix, and erases the field if it's not correct.
+   *
+   * @return void
+   */
+  protected function loadFormData()
+  {
+    $app              = Factory::getApplication();
+    $filters          = $app->getUserState($this->context . '.filter', array());
+    $error_dateformat = false;
+
+    foreach ($filters as $key => $value)
     {
-        $items = parent::getItems();
-        $params = $this->state->get('parameters.menu');
-        $pattern = $params['citation_style_id'][0];
-        return PubdbLiteraturesCitation::mapList($pattern, $items);
+      if (strpos($key, '_dateformat') && !empty($value) && $this->isValidDate($value) == null)
+      {
+        $filters[$key]    = '';
+        $error_dateformat = true;
+      }
     }
 
-    /**
-     * Overrides the default function to check Date fields format, identified by
-     * "_dateformat" suffix, and erases the field if it's not correct.
-     *
-     * @return void
-     */
-    protected function loadFormData()
+    if ($error_dateformat)
     {
-        $app              = Factory::getApplication();
-        $filters          = $app->getUserState($this->context . '.filter', array());
-        $error_dateformat = false;
-
-        foreach ($filters as $key => $value)
-        {
-            if (strpos($key, '_dateformat') && !empty($value) && $this->isValidDate($value) == null)
-            {
-                $filters[$key]    = '';
-                $error_dateformat = true;
-            }
-        }
-
-        if ($error_dateformat)
-        {
-            $app->enqueueMessage(Text::_("COM_PUBDB_SEARCH_FILTER_DATE_FORMAT"), "warning");
-            $app->setUserState($this->context . '.filter', $filters);
-        }
-
-        return parent::loadFormData();
+      $app->enqueueMessage(Text::_("COM_PUBDB_SEARCH_FILTER_DATE_FORMAT"), "warning");
+      $app->setUserState($this->context . '.filter', $filters);
     }
 
-    /**
-     * Checks if a given date is valid and in a specified format (YYYY-MM-DD)
-     *
-     * @param   string  $date  Date to be checked
-     *
-     * @return bool
-     */
-    private function isValidDate($date)
-    {
-        $date = str_replace('/', '-', $date);
-        return (date_create($date)) ? Factory::getDate($date)->format("Y-m-d") : null;
-    }
+    return parent::loadFormData();
+  }
+
+  /**
+   * Checks if a given date is valid and in a specified format (YYYY-MM-DD)
+   *
+   * @param   string  $date  Date to be checked
+   *
+   * @return bool
+   */
+  private function isValidDate($date)
+  {
+    $date = str_replace('/', '-', $date);
+    return (date_create($date)) ? Factory::getDate($date)->format("Y-m-d") : null;
+  }
 }

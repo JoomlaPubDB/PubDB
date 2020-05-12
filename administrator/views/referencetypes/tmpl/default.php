59e522c71d5b200045b204ae1c34fb83892f2744
FloFreak <minecraftfreak1210@gmail.com>
2020-05-12 14:51:16 +0200

    Reference type show now name in admin view
    Signed-off-by: FloFreak <minecraftfreak1210@gmail.com>

--- administrator/views/referencetypes/tmpl/default.php
+++ administrator/views/referencetypes/tmpl/default.php
@@ -28,149 +28,156 @@ $document = Factory::getDocument();
 $document->addStyleSheet(Uri::root() . 'administrator/components/com_pubdb/assets/css/pubdb.css');
 $document->addStyleSheet(Uri::root() . 'media/com_pubdb/css/list.css');
 
-$user      = Factory::getUser();
-$userId    = $user->get('id');
+$user = Factory::getUser();
+$userId = $user->get('id');
 $listOrder = $this->state->get('list.ordering');
-$listDirn  = $this->state->get('list.direction');
-$canOrder  = $user->authorise('core.edit.state', 'com_pubdb');
+$listDirn = $this->state->get('list.direction');
+$canOrder = $user->authorise('core.edit.state', 'com_pubdb');
 $saveOrder = $listOrder == 'a.`ordering`';
 
-if ($saveOrder)
-{
-	$saveOrderingUrl = 'index.php?option=com_pubdb&task=referencetypes.saveOrderAjax&tmpl=component';
-    HTMLHelper::_('sortablelist.sortable', 'referencetypeList', 'adminForm', strtolower($listDirn), $saveOrderingUrl);
+if ($saveOrder) {
+  $saveOrderingUrl = 'index.php?option=com_pubdb&task=referencetypes.saveOrderAjax&tmpl=component';
+  HTMLHelper::_('sortablelist.sortable', 'referencetypeList', 'adminForm', strtolower($listDirn), $saveOrderingUrl);
 }
 
 $sortFields = $this->getSortFields();
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_pubdb&view=referencetypes'); ?>" method="post"
-	  name="adminForm" id="adminForm">
-	<?php if (!empty($this->sidebar)): ?>
-	<div id="j-sidebar-container" class="span2">
-		<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-		<?php else : ?>
-		<div id="j-main-container">
-			<?php endif; ?>
+      name="adminForm" id="adminForm">
+  <?php if (!empty($this->sidebar)): ?>
+    <div id="j-sidebar-container" class="span2">
+      <?php echo $this->sidebar; ?>
+    </div>
+    <div id="j-main-container" class="span10">
+      <?php else : ?>
+        <div id="j-main-container">
+          <?php endif; ?>
 
-            <?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
+          <?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 
-			<div class="clearfix"></div>
-			<table class="table table-striped" id="referencetypeList">
-				<thead>
-				<tr>
-					<?php if (isset($this->items[0]->ordering)): ?>
-						<th width="1%" class="nowrap center hidden-phone">
-                            <?php echo HTMLHelper::_('searchtools.sort', '', 'a.`ordering`', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-menu-2'); ?>
-                        </th>
-					<?php endif; ?>
-					<th width="1%" class="hidden-phone">
-						<input type="checkbox" name="checkall-toggle" value=""
-							   title="<?php echo Text::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)"/>
-					</th>
-					<?php if (isset($this->items[0]->state)): ?>
-						<th width="1%" class="nowrap center">
-								<?php echo JHtml::_('searchtools.sort', 'JSTATUS', 'a.`state`', $listDirn, $listOrder); ?>
-</th>
-					<?php endif; ?>
+            <div class="clearfix"></div>
+            <table class="table table-striped" id="referencetypeList">
+                <thead>
+                <tr>
+                  <?php if (isset($this->items[0]->ordering)): ?>
+                      <th width="1%" class="nowrap center hidden-phone">
+                        <?php echo HTMLHelper::_('searchtools.sort', '', 'a.`ordering`', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-menu-2'); ?>
+                      </th>
+                  <?php endif; ?>
+                    <th width="1%" class="hidden-phone">
+                        <input type="checkbox" name="checkall-toggle" value=""
+                               title="<?php echo Text::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)"/>
+                    </th>
+                  <?php if (isset($this->items[0]->state)): ?>
+                      <th width="1%" class="nowrap center">
+                        <?php echo JHtml::_('searchtools.sort', 'JSTATUS', 'a.`state`', $listDirn, $listOrder); ?>
+                      </th>
+                  <?php endif; ?>
 
-									<th class='left'>
-				<?php echo JHtml::_('searchtools.sort',  'COM_PUBDB_REFERENCETYPES_ID', 'a.`id`', $listDirn, $listOrder); ?>
-				</th>
-				<th class='left'>
-				<?php echo JHtml::_('searchtools.sort',  'COM_PUBDB_REFERENCETYPES_LABLE', 'a.`lable`', $listDirn, $listOrder); ?>
-				</th>
+                    <th class='left'>
+                      <?php echo JHtml::_('searchtools.sort', 'COM_PUBDB_REFERENCETYPES_ID', 'a.`id`', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class='left'>
+                      <?php echo JHtml::_('searchtools.sort', 'COM_PUBDB_REFERENCETYPES_NAME', 'a.`name`', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class='left'>
+                      <?php echo JHtml::_('searchtools.sort', 'COM_PUBDB_REFERENCETYPES_LABLE', 'a.`lable`', $listDirn, $listOrder); ?>
+                    </th>
 
-					
-				</tr>
-				</thead>
-				<tfoot>
-				<tr>
-					<td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
-						<?php echo $this->pagination->getListFooter(); ?>
-					</td>
-				</tr>
-				</tfoot>
-				<tbody>
-				<?php foreach ($this->items as $i => $item) :
-					$ordering   = ($listOrder == 'a.ordering');
-					$canCreate  = $user->authorise('core.create', 'com_pubdb');
-					$canEdit    = $user->authorise('core.edit', 'com_pubdb');
-					$canCheckin = $user->authorise('core.manage', 'com_pubdb');
-					$canChange  = $user->authorise('core.edit.state', 'com_pubdb');
-					?>
-					<tr class="row<?php echo $i % 2; ?>">
 
-						<?php if (isset($this->items[0]->ordering)) : ?>
-							<td class="order nowrap center hidden-phone">
-								<?php if ($canChange) :
-									$disableClassName = '';
-									$disabledLabel    = '';
+                </tr>
+                </thead>
+                <tfoot>
+                <tr>
+                    <td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
+                      <?php echo $this->pagination->getListFooter(); ?>
+                    </td>
+                </tr>
+                </tfoot>
+                <tbody>
+                <?php foreach ($this->items as $i => $item) :
+                  $ordering = ($listOrder == 'a.ordering');
+                  $canCreate = $user->authorise('core.create', 'com_pubdb');
+                  $canEdit = $user->authorise('core.edit', 'com_pubdb');
+                  $canCheckin = $user->authorise('core.manage', 'com_pubdb');
+                  $canChange = $user->authorise('core.edit.state', 'com_pubdb');
+                  ?>
+                    <tr class="row<?php echo $i % 2; ?>">
 
-									if (!$saveOrder) :
-										$disabledLabel    = Text::_('JORDERINGDISABLED');
-										$disableClassName = 'inactive tip-top';
-									endif; ?>
-									<span class="sortable-handler hasTooltip <?php echo $disableClassName ?>"
-										  title="<?php echo $disabledLabel ?>">
+                      <?php if (isset($this->items[0]->ordering)) : ?>
+                          <td class="order nowrap center hidden-phone">
+                            <?php if ($canChange) :
+                              $disableClassName = '';
+                              $disabledLabel = '';
+
+                              if (!$saveOrder) :
+                                $disabledLabel = Text::_('JORDERINGDISABLED');
+                                $disableClassName = 'inactive tip-top';
+                              endif; ?>
+                                <span class="sortable-handler hasTooltip <?php echo $disableClassName ?>"
+                                      title="<?php echo $disabledLabel ?>">
 							<i class="icon-menu"></i>
 						</span>
-									<input type="text" style="display:none" name="order[]" size="5"
-										   value="<?php echo $item->ordering; ?>" class="width-20 text-area-order "/>
-								<?php else : ?>
-									<span class="sortable-handler inactive">
+                                <input type="text" style="display:none" name="order[]" size="5"
+                                       value="<?php echo $item->ordering; ?>" class="width-20 text-area-order "/>
+                            <?php else : ?>
+                                <span class="sortable-handler inactive">
 							<i class="icon-menu"></i>
 						</span>
-								<?php endif; ?>
-							</td>
-						<?php endif; ?>
-						<td class="hidden-phone">
-							<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
-						</td>
-						<?php if (isset($this->items[0]->state)): ?>
-							<td class="center">
-								<?php echo JHtml::_('jgrid.published', $item->state, $i, 'referencetypes.', $canChange, 'cb'); ?>
-</td>
-						<?php endif; ?>
+                            <?php endif; ?>
+                          </td>
+                      <?php endif; ?>
+                        <td class="hidden-phone">
+                          <?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
+                        </td>
+                      <?php if (isset($this->items[0]->state)): ?>
+                          <td class="center">
+                            <?php echo JHtml::_('jgrid.published', $item->state, $i, 'referencetypes.', $canChange, 'cb'); ?>
+                          </td>
+                      <?php endif; ?>
 
-										<td>
+                        <td>
 
-					<?php echo $item->id; ?>
-				</td>				<td>
-				<?php if (isset($item->checked_out) && $item->checked_out && ($canEdit || $canChange)) : ?>
-					<?php echo JHtml::_('jgrid.checkedout', $i, $item->uEditor, $item->checked_out_time, 'referencetypes.', $canCheckin); ?>
-				<?php endif; ?>
-				<?php if ($canEdit) : ?>
-					<a href="<?php echo JRoute::_('index.php?option=com_pubdb&task=referencetype.edit&id='.(int) $item->id); ?>">
-					<?php echo $this->escape($item->lable); ?></a>
-				<?php else : ?>
-					<?php echo $this->escape($item->lable); ?>
-				<?php endif; ?>
+                          <?php echo $item->id; ?>
+                        </td>
+                        <td>
+                          <?php if (isset($item->checked_out) && $item->checked_out && ($canEdit || $canChange)) : ?>
+                            <?php echo JHtml::_('jgrid.checkedout', $i, $item->uEditor, $item->checked_out_time, 'referencetypes.', $canCheckin); ?>
+                          <?php endif; ?>
+                          <?php if ($canEdit) : ?>
+                              <a href="<?php echo JRoute::_('index.php?option=com_pubdb&task=referencetype.edit&id=' . (int)$item->id); ?>">
+                                <?php echo $this->escape($item->name); ?></a>
+                          <?php else : ?>
+                            <?php echo $this->escape($item->name); ?>
+                          <?php endif; ?>
 
-				</td>
+                        </td>
+                        <td>
 
-					</tr>
-				<?php endforeach; ?>
-				</tbody>
-			</table>
+                          <?php echo $this->escape($item->lable); ?>
+                        </td>
 
-			<input type="hidden" name="task" value=""/>
-			<input type="hidden" name="boxchecked" value="0"/>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+
+            <input type="hidden" name="task" value=""/>
+            <input type="hidden" name="boxchecked" value="0"/>
             <input type="hidden" name="list[fullorder]" value="<?php echo $listOrder; ?> <?php echo $listDirn; ?>"/>
-			<?php echo HTMLHelper::_('form.token'); ?>
-		</div>
+          <?php echo HTMLHelper::_('form.token'); ?>
+        </div>
 </form>
 <script>
     window.toggleField = function (id, task, field) {
 
-        var f = document.adminForm, i = 0, cbx, cb = f[ id ];
+        var f = document.adminForm, i = 0, cbx, cb = f[id];
 
         if (!cb) return false;
 
         while (true) {
-            cbx = f[ 'cb' + i ];
+            cbx = f['cb' + i];
 
             if (!cbx) break;
 
@@ -178,10 +185,10 @@ $sortFields = $this->getSortFields();
             i++;
         }
 
-        var inputField   = document.createElement('input');
+        var inputField = document.createElement('input');
 
-        inputField.type  = 'hidden';
-        inputField.name  = 'field';
+        inputField.type = 'hidden';
+        inputField.name = 'field';
         inputField.value = field;
         f.appendChild(inputField);
 

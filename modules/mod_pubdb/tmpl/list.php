c8d528f269e125a236983598d43351f84df19882
RobertStrobel <team@gmx.de>
2020-05-22 19:05:37 +0200

    added labels for pubDB module backend

--- modules/mod_pubdb/tmpl/list.php
+++ modules/mod_pubdb/tmpl/list.php
@@ -10,20 +10,18 @@
 defined('_JEXEC') or die;
 $elements = ModPubdbHelper::getList($params);
 
-$tableField = explode(':', $params->get('field'));
-$table_name = !empty($tableField[0]) ? $tableField[0] : '';
-$field_name = !empty($tableField[1]) ? $tableField[1] : '';
 ?>
 
 <?php if (!empty($elements)) : ?>
-	<table class="table">
-		<?php foreach ($elements as $element) : ?>
-			<tr>
-				<th><?php echo ModPubdbHelper::renderTranslatableHeader($table_name, $field_name); ?></th>
-				<td><?php echo ModPubdbHelper::renderElement(
-						$table_name, $params->get('field'), $element->{$field_name}
-					); ?></td>
-			</tr>
-		<?php endforeach; ?>
-	</table>
+    <table class="table">
+        <thead>
+        <th><tr></tr></th>
+        </thead>
+
+        <?php foreach ($elements as $element) : ?>
+            <tr>
+                <td><?php echo $element; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 <?php endif;
